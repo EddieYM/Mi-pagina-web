93e91b67881c99b926fe0173e1d36d84b9554337
EddieYM <r01094a@upla.edu.pe>
2025-03-29 16:40:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("mobile-menu");
+    const navList = document.querySelector(".nav-list");
+
+    menuToggle.addEventListener("click", () => {
+        navList.classList.toggle("active");
+    });
+});
